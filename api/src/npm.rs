f666682ab2f0bbff4e71160fc200f8ece074095b
Sam Denty <sam@samdenty.com>
2022-12-29 16:35:28 +0000

    fix(api): improve npm resolver speed

--- api/src/npm.rs
+++ api/src/npm.rs
@@ -1,6 +1,7 @@
+use futures::{future::select_all, Future, FutureExt};
 use regex::Regex;
 use serde_json::Value;
-use std::error::Error;
+use std::{error::Error, pin::Pin};
 use worker::{Fetch, Url};
 
 const NODEJS_BUILTINS: [&str; 41] = [
@@ -70,6 +71,53 @@ pub async fn get_redirect_url(mut url: Url, package_name: &str) -> Result<Url, B
 }
 
 pub async fn get_slug(package_name: &str) -> Result<String, Box<dyn Error>> {
+  let mut futures: Vec<Pin<Box<dyn Future<Output = Result<String, Box<dyn Error>>>>>> = vec![
+    npm_resolver(package_name).boxed_local(),
+    npms_resolver(package_name).boxed_local(),
+  ];
+
+  loop {
+    let (repository_url, index, _) = select_all(&mut futures).await;
+    futures.remove(index);
+
+    if let Ok(repository_url) = repository_url {
+      let re = Regex::new(r"github(\.com)?[/:]([^/]+/[^/]+)").unwrap();
+      let captures = re
+        .captures(&repository_url)
+        .ok_or("not a github repository")?;
+
+      let slug = captures[2].trim_end_matches(".git").to_lowercase();
+      return Ok(slug);
+    }
+
+    if futures.is_empty() {
+      return Err(repository_url.unwrap_err());
+    }
+  }
+}
+
+async fn npms_resolver(package_name: &str) -> Result<String, Box<dyn Error>> {
+  let resp: Value = Fetch::Url(
+    format!("https://api.npms.io/v2/package/{}", package_name)
+      .parse()
+      .unwrap(),
+  )
+  .send()
+  .await?
+  .json()
+  .await?;
+
+  if let Value::String(message) = &resp["message"] {
+    return Err(message.clone().into());
+  }
+
+  resp["collected"]["metadata"]["links"]["repository"]
+    .as_str()
+    .map(|repo| repo.to_string())
+    .ok_or("no repository field specified".into())
+}
+
+async fn npm_resolver(package_name: &str) -> Result<String, Box<dyn Error>> {
   let resp: Value = Fetch::Url(
     format!("https://registry.npmjs.org/{}/latest", package_name)
       .parse()
@@ -82,18 +130,10 @@ pub async fn get_slug(package_name: &str) -> Result<String, Box<dyn Error>> {
 
   let repository = &resp["repository"];
 
-  let repository_url = match repository {
-    Value::String(s) => &s,
-    Value::Object(o) => o["url"].as_str().unwrap(),
-    Value::Null => return Err(format!("no repository field specified").into()),
-    value => return Err(format!("unexpected npm response {:?}", value).into()),
-  };
-
-  let re = Regex::new(r"github(\.com)?[/:]([^/]+/[^/]+)").unwrap();
-  let captures = re
-    .captures(repository_url)
-    .ok_or("not a github repository")?;
-
-  let slug = captures[2].trim_end_matches(".git").to_lowercase();
-  Ok(slug)
+  match repository {
+    Value::String(s) => Ok(s.to_string()),
+    Value::Object(o) => Ok(o["url"].as_str().unwrap().to_string()),
+    Value::Null => Err(format!("no repository field specified").into()),
+    value => Err(format!("unexpected npm response {:?}", value).into()),
+  }
 }
